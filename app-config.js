8eb222d3507a169a4af28ac071e6091d65d4b6f6
GISPHN <spi4949@gmail.com>
2026-06-17 09:29:27 +0900

    Switch config to Google Sheets mode

--- app-config.js
+++ app-config.js
@@ -1,7 +1,5 @@
 window.GISPHN_CONFIG = {
-  mode: "local-demo",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  mode: "gas-sheet",
   gasWebAppUrl: "https://script.google.com/macros/s/AKfycbw9nmZvzYeElaqzdJl3o-2QMo3NAw0QPhpCzVZuAXiUJxQc24xNKEckuPkhDzpI9Hzg/exec",
   pollIntervalMs: 30000,
 };
