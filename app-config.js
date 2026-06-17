54a44cda6bdace9e2ded03e858b5bc3d68e20397
GISPHN <spi4949@gmail.com>
2026-06-17 09:01:17 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@ window.GISPHN_CONFIG = {
   mode: "local-demo",
   supabaseUrl: "",
   supabaseAnonKey: "",
-  gasWebAppUrl: "",
+  gasWebAppUrl: "https://script.google.com/macros/s/AKfycbw9nmZvzYeElaqzdJl3o-2QMo3NAw0QPhpCzVZuAXiUJxQc24xNKEckuPkhDzpI9Hzg/exec",
   pollIntervalMs: 30000,
 };
